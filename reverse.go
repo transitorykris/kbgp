c1344aed5464c8a2125eb64b3111faad929d9edc
Kris Foster <kris.foster@gmail.com>
2017-12-02 19:21:13 -0800

    Add reverse name lookups for the remaining enumerations

--- reverse.go
+++ reverse.go
@@ -38,3 +38,70 @@ var stateName = map[int]string{
 	3: "OpenConfirm",
 	4: "Established",
 }
+
+var messageName = map[int]string{
+	1: "OPEN",
+	2: "UPDATE",
+	3: "NOTIFICATION",
+	4: "KEEPALIVE",
+	//5: "ROUTE-REFRESH" // [RFC2918] defines one more type code.
+}
+
+var originName = map[int]string{
+	0: "IGP",
+	1: "EGP",
+	2: "INCOMPLETE",
+}
+
+var pathAttributeName = map[int]string{
+	1: "ORIGIN",
+	2: "AS_PATH",
+	3: "NEXT_HOP",
+	4: "MULTI_EXIT_DISC",
+	5: "LOCAL_PREF",
+	6: "ATOMIC_AGGREGATE",
+	7: "AGGREGATOR",
+}
+
+var asPathName = map[int]string{
+	1: "AS_SET",
+	2: "AS_SEQUENCE",
+}
+
+var errorCodeName = map[int]string{
+	1: "Message Header Error",
+	2: "OPEN Message Error",
+	3: "UPDATE Message Error",
+	4: "Hold Timer Expired",
+	5: "Finite State Machine Error",
+	6: "Cease",
+}
+
+var messageHeaderErrorSubcodeName = map[int]string{
+	1: "Connection Not Synchronized",
+	2: "Bad Message Length",
+	3: "Bad Message Type",
+}
+
+var openMessageErrorSubcodeName = map[int]string{
+	1: "Unsupported Version Number",
+	2: "Bad Peer AS",
+	3: "Bad BGP Identifier",
+	4: "Unsupported Optional Parameter",
+	// 5 is deprecated
+	6: "Unacceptable Hold Time",
+}
+
+var updateMessageErrorSubcodeName = map[int]string{
+	1: "Malformed Attribute List",
+	2: "Unrecognized Well-known Attribute",
+	3: "Missing Well-known Attribute",
+	4: "Attribute Flags Error",
+	5: "Attribute Length Error",
+	6: "Invalid ORIGIN Attribute",
+	// 7 is deprecated
+	8:  "Invalid NEXT_HOP Attribute",
+	9:  "Optional Attribute Error",
+	10: "Invalid Network Field",
+	11: "Malformed AS_PATH",
+}
