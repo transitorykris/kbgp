00fc3206a21ba2f6c2c883de9d11c8be9508a28f
Kris Foster <kris.foster@gmail.com>
2017-12-02 16:58:30 -0800

    Give network tests their own test file

---
+++ network_test.go
@@ -0,0 +1,23 @@
+package kbgp
+
+import (
+	"encoding/binary"
+	"net"
+	"testing"
+)
+
+func TestFindBGPIdentifier(t *testing.T) {
+	_, err := FindBGPIdentifier()
+	if err != nil {
+		t.Errorf("Unexpected error guessing IP: %v", err)
+	}
+}
+
+func TestIPToBGPIdentifier(t *testing.T) {
+	ip := net.ParseIP("1.2.3.4")
+	id := ipToUint32(ip)
+	ip4 := binary.BigEndian.Uint32(ip.To4())
+	if ip4 != id {
+		t.Errorf("Incorrect identifier %d != %d", ip4, id)
+	}
+}
