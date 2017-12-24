5dc2bab99b42661a44e1633aac4341a47625bcd0
Kris Foster <kris.foster@gmail.com>
2017-12-23 20:10:48 -0800

    Starting fresh from the developer's perspective

--- cmd/main.go
+++ cmd/main.go
@@ -1,38 +1,9 @@
 package main
 
-import (
-	"log"
-	"net"
-
-	"github.com/transitorykris/kbgp"
-)
+import "log"
 
 func main() {
-	log.Println("Starting kBGP")
+	log.Println("Starting")
 
-	// Config
-	myAS := uint16(1234)
-	id, err := kbgp.FindBGPIdentifier()
-	if err != nil {
-		panic(err)
-	}
-
-	// Start our router
-	log.Printf("AS: %d ID: %s", myAS, kbgp.Uint32ToIP(id).String())
-	bgp := kbgp.New(myAS, id)
-
-	remoteAS := uint16(1001)
-	remoteIP := net.ParseIP("127.0.0.1")
-	if err := bgp.AddPeer(remoteAS, remoteIP); err != nil {
-		log.Fatal(err)
-	}
-
-	bgp.Start()
-
-	select {}
-
-	//time.Sleep(5 * time.Second)
-
-	//bgp.Stop()
-	//log.Println("Exiting")
+	log.Println("Exiting")
 }
