cae98b9a782bf084c56e16bdf4f8530548ec70c3
Kris Foster <kris.foster@gmail.com>
2017-12-26 17:28:29 -0800

    Create a new BGP speaker

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"log"
+
+	"github.com/transitorykris/kbgp"
 )
 
 func main() {
 	log.Println("Starting kBGP")
+
+	router, err := kbgp.New(1234, "1.2.3.4")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Configured speaker AS%s/%s", router.MyAS(), router.BGPIdentifier())
+
+	log.Println("Exiting kBGP")
 }
