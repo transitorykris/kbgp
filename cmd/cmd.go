87aedb851ac656e8ff75b9f1abb6a952235342f3
Kris Foster <kris.foster@gmail.com>
2018-01-05 23:26:05 -0800

    Add a client to create a new speaker with a peer

---
+++ cmd/cmd.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/transitorykris/jbgp"
+)
+
+func main() {
+	log.Println("Creating a new speaker")
+	speaker := jbgp.NewSpeaker(":179")
+
+	log.Println("Adding a peer")
+	speaker.Peer(jbgp.NewPeer(1234, net.ParseIP("1.2.3.4")))
+
+	log.Println("Starting the speaker")
+	speaker.Start()
+
+	log.Println("Exiting jBGP")
+}
