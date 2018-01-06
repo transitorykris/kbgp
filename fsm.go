d9643c7aea5a11313f9804678c66a01cf7b538dd
Kris Foster <kris.foster@gmail.com>
2018-01-05 23:51:40 -0800

    Add nice string logging for events and state transitions

--- fsm.go
+++ fsm.go
@@ -13,6 +13,20 @@ const (
 	established
 )
 
+var stateLookup = map[state]string{
+	idle:        "Idle",
+	connect:     "Connect",
+	active:      "Active",
+	openSent:    "OpenSent",
+	openConfirm: "OpenConfirm",
+	established: "Established",
+}
+
+// String implements string.Stringer
+func (s state) String() string {
+	return stateLookup[s]
+}
+
 type fsm struct {
 	state state
 
@@ -60,8 +74,61 @@ const (
 	UpdateMsgErr
 )
 
+var eventLookup = map[event]string{
+	ManualStart:                                                      "ManualStart",
+	ManualStop:                                                       "ManualStop",
+	ManualStartWithPassiveTCPEstablishment:                           "ManualStart_with_PassiveTcpEstablishment",
+	AutomaticStartWithPassiveTCPEstablishment:                        "AutomaticStart_with_PassiveTcpEstablishment",
+	AutomaticStartWithDampPeerOscillations:                           "AutomaticStart_with_DampPeerOscillations",
+	AutomaticStartWithDampPeerOscillationsAndPassiveTCPEstablishment: "AutomaticStart_with_DampPeerOscillations_and_PassiveTcpEstablishment",
+	AutomaticStop:            "AutomaticStop",
+	ConnectRetryTimerExpires: "ConnectRetryTimer_Expires",
+	HoldTimerExpires:         "HoldTimer_Expires",
+	KeepaliveTimerExpires:    "KeepaliveTimer_Expires",
+	DelayOpenTimerExpires:    "DelayOpenTimer_Expires",
+	IdleHoldTimerExpires:     "IdleHoldTimer_Expires",
+	TCPConnectionValid:       "TcpConnection_Valid",
+	TCPCRAcked:               "Tcp_CR_Acked",
+	TCPConnectionConfirmed:   "TcpConnectionConfirmed",
+	TCPConnectionFails:       "TcpConnectionFails",
+	BGPOpen:                  "BGPOpen",
+	BGPOpenWithDelayOpenTimerRunning: "BGPOpen with DelayOpenTimer running",
+	BGPHeaderErr:                     "BGPHeaderErr",
+	BGPOpenMsgErr:                    "BGPOpenMsgErr",
+	OpenCollisionDump:                "OpenCollisionDump",
+	NotifMsgVerErr:                   "NotifMsgVerErr",
+	NotifMsg:                         "NotifMsg",
+	KeepAliveMsg:                     "KeepAliveMsg",
+	UpdateMsg:                        "UpdateMsg",
+	UpdateMsgErr:                     "UpdateMsgErr",
+}
+
+// String implements string.Stringer
+func (e event) String() string {
+	return eventLookup[e]
+}
+
 func (f *fsm) event(e event) {
 	log.Println("routing event", e, "to state", f.state)
+	switch f.state {
+	case idle:
+		f.idle(e)
+	case connect:
+		f.connect(e)
+	case active:
+		f.active(e)
+	case openSent:
+		f.openSent(e)
+	case openConfirm:
+		f.openConfirm(e)
+	case established:
+		f.established(e)
+	}
+}
+
+func (f *fsm) transition(s state) {
+	log.Println("Transitioning from", f.state, "to", s)
+	f.state = s
 }
 
 func (f *fsm) idle(e event) {
